93659cb95d98e16aec90bb6f8ef5bb2d1ab83a53
Bruno Boehm <bruno.boehm@gmail.com>
2013-10-16 18:56:45 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marmelad::Application.config.secret_key_base = '32d7ec8a1f8c49922f4a089288f600f4eaba54776f5f4153f12d2682f3fad571d7665dfe072c6aca61dade579b66b18bbdfd0dcdea4040e54e859296c5ad7091'
