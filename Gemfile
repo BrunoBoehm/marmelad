627a032067bd714c958be2900da0bbeb875a55e9
Bruno Boehm <bruno.boehm@gmail.com>
2014-05-23 16:53:29 +0200

    start design

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
+gem 'devise'
+
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
@@ -23,6 +25,7 @@ end
 group :production do
   gem 'pg','0.15.1'
   gem 'rails_12factor','0.0.2'
+  #rails12factor for heroku
 end
 
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Marmelad::Application.routes.draw do
-  get "home/index"
+  devise_for :users
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root to: 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
